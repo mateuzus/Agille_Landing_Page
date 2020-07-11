34abb904aac402704be5f447397e4849cd7596df
Mateus Santos <mateusdsmarcelino@gmail.com>
2020-07-11 02:01:14 -0300

    Remove scroll.js

--- js/scroll.js
+++
@@ -1,9 +0,0 @@
-$('ul a').click(function(e){
-	e.preventDefault();
-	var id = $(this).attr('href'),
-    targetOffset = $(id).offset().top,
-    menuHeight = $('ul a').innerHeight();
-	$('html, body').animate({
-		scrollTop: targetOffset - menuHeight
-	}, 500);	
-});
